fa72b19a4024b2b88b296209de64d1b27de3ec72
nkoshell <nikita.koshelev@gmail.com>
2017-11-13 12:19:25 +0300

    Refactor class-based views

--- backend/app.py
+++ backend/app.py
@@ -1,40 +1,14 @@
 from flask import Flask
 from playhouse.flask_utils import FlaskDB
 
-from backend.models import db
-from backend.views import BookModelApiView, CategoryModelApiView
+from backend.models import db, Book, Category
+from backend.views import ApiModelView
 
 app = Flask(__name__)
 db_wrapper = FlaskDB(app=app, database=db)
 
-book_view = BookModelApiView.as_view('books')
-category_view = CategoryModelApiView.as_view('categories')
-
-app.add_url_rule(
-    rule='/books/',
-    view_func=book_view,
-    defaults={'book_id': None},
-    methods=['GET']
-)
-
-app.add_url_rule(
-    rule='/books/<int:book_id>',
-    view_func=book_view,
-    methods=['GET']
-)
-app.add_url_rule(
-    rule='/categories/',
-    view_func=category_view,
-    defaults={'category_id': None},
-    methods=['GET']
-)
-
-app.add_url_rule(
-    rule='/categories/<int:category_id>',
-    view_func=category_view,
-    methods=['GET']
-)
-
+ApiModelView.register(app, Book, endpoint='books')
+ApiModelView.register(app, Category, endpoint='categories')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/views.py
+++ backend/views.py
@@ -1,44 +1,102 @@
-from flask.views import MethodView
-from playhouse.shortcuts import model_to_dict
-from werkzeug.exceptions import abort
+import copy
 
-from backend.models import Book, Category
+from flask import abort, request
+from flask.views import MethodView
+from peewee import Model
+from playhouse.shortcuts import model_to_dict
+
 from backend.utils import json_response
 
 
-class BookModelApiView(MethodView):
+class BaseModelView(MethodView):
+    _model = None
+
+    def __init__(self, model=None):
+        self.check_model(model)
+        self._model = model
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def model_meta(self):
+        return self.get_model_meta(self.model)
+
+    @staticmethod
+    def check_model(model):
+        if not issubclass(model, Model):
+            raise ValueError('Arg `model` must be `peewee.Model` subclass, got %r' % model)
+        return model
+
+    @staticmethod
+    def get_model_meta(model):
+        return copy.copy(model._meta)
+
+    @classmethod
+    def get_url_rules(cls, model, endpoint=None, url=None, pk='instance_id', pk_type='int',
+                      *cls_args, **cls_kwargs):
+        cls.check_model(model)
+        if endpoint is None:
+            model_meta = cls.get_model_meta(model)
+            endpoint = model_meta.name
+        if url is None:
+            url = '/%s/' % endpoint
+
+        single_resource_url = '%(url)s/<%(pk_type)s:%(pk)s>/' % dict(
+            url=url.rstrip('/'),
+            pk=pk,
+            pk_type=pk_type
+        )
+
+        view_func = cls.as_view(endpoint, model=model, *cls_args, **cls_kwargs)
+
+        # Ordering
+        url_rules = [
+            {'rule': url, 'view_func': view_func, 'defaults': {pk: None}, 'methods': ['GET', ]},
+            {'rule': url.rstrip('/'), 'view_func': view_func, 'methods': ['POST', ]},
+            {'rule': url, 'view_func': view_func, 'methods': ['POST', ]},
+            {'rule': single_resource_url.rstrip('/'), 'view_func': view_func, 'methods': ['GET', 'PUT', 'DELETE', ]},
+            {'rule': single_resource_url, 'view_func': view_func, 'methods': ['GET', 'PUT', 'DELETE', ]},
+        ]
+        return url_rules
+
+    @classmethod
+    def register(cls, app, model, *args, **kwargs):
+        url_rules = cls.get_url_rules(model, *args, **kwargs)
+        for url_rule in url_rules:
+            app.add_url_rule(**url_rule)
+
+
+class ApiModelView(BaseModelView):
     decorators = [
         json_response,
     ]
 
-    def list(self):  # Shortcut
-        return [model_to_dict(book) for book in Book.select()]
-
-    def get(self, book_id):
-        if book_id is not None:
+    def get(self, instance_id=None):
+        if instance_id is not None:
             try:
-                book = Book.get(Book.id == book_id)
-            except Book.DoesNotExist:
-                abort(404, 'Book with id=%r does not exists' % book_id)
+                instance = self.model.get(self.model_meta.primary_key == instance_id)
+            except self.model.DoesNotExist:
+                abort(404, '%s with id=%r does not exist' % (self.model_meta.name, instance_id))
             else:
-                return model_to_dict(book, backrefs=True)
+                return model_to_dict(instance)
         return self.list()
 
+    def list(self):
+        query = self.model.select()
+        filters = self.get_filters()
+        if filters:
+            query = query.filter(**filters)
+        instances = [model_to_dict(instance) for instance in query]
+        return instances
 
-class CategoryModelApiView(MethodView):
-    decorators = [
-        json_response,
-    ]
-
-    def list(self):  # Shortcut
-        return [model_to_dict(category) for category in Category.select()]
-
-    def get(self, category_id):
-        if category_id is not None:
-            try:
-                category = Category.get(Category.id == category_id)
-            except Book.DoesNotExist:
-                abort(404, 'Category with id=%r does not exists' % category_id)
-            else:
-                return model_to_dict(category, backrefs=True)
-        return self.list()
+    @staticmethod
+    def get_filters():
+        filters = {}
+        for k, v in request.args.items():
+            if '__' in k:
+                if k.endswith('in'):
+                    v = v.split(',')
+                filters[k] = v
+        return filters
