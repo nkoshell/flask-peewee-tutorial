a9e36fcb22517b78e6667c06cc8a7f1c037bd0b6
nkoshell <nikita.koshelev@gmail.com>
2017-11-14 13:33:47 +0300

    Added search for books

--- frontend/js/books/app.js
+++ frontend/js/books/app.js
@@ -1,8 +1,30 @@
 (function ($) {
-   var apiUrl = window.apiUrl || 'http://localhost:5000/'; // Get from global ctx or set to 'http://localhost:5000/'
+    var apiUrl = window.apiUrl || 'http://localhost:5000/'; // Get from global ctx or set to 'http://localhost:5000/'
 
-    $.getJSON(apiUrl + 'books').done(function (data) {
-        console.log('called');
-        $("#template-container").loadTemplate("/templates/book.tpl.html", data);
+    // call on page loading
+    loadBooks();
+
+    var $searchForm = $('#searchBookByTitle');
+
+    $searchForm.submit(function (evt) {
+        evt.preventDefault();
+        var data = $searchForm.serializeArray();
+        var params = {};
+
+        data.forEach(function (element, index) {
+            params[element.name] = element.value;
+        })
+
+        loadBooks(params);
     });
+
+
+    function loadBooks(params) {
+        params = params || {};
+        $.getJSON(apiUrl + 'books', params).done(function (data) {
+            console.log('called');
+            $("#template-container").loadTemplate("/templates/book.tpl.html", data);
+        });
+    }
+
 })(window.jQuery);
